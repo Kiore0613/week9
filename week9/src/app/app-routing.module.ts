4edbacd72234a98d088639e909ce8c6f9f2a3837
Kiore <ricardochicas95@gmail.com>
2020-01-13 14:21:07 -0600

    config routes

--- week9/src/app/app-routing.module.ts
+++ week9/src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { NotFoundComponent } from "./modules/shared/components/not-found/not-found.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home"
+  },
+  {
+    path: "home",
+    loadChildren: () => import("./modules/main-layout/main-layout.module")
+  },
+  {
+    path: "authentication",
+    loadChildren: () => import("./modules/authentication/authentication.module")
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
