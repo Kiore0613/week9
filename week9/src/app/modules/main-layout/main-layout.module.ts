3bc74f121034d80ccabb4fb83f18163db49df51a
Kiore <ricardochicas95@gmail.com>
2020-01-13 13:42:31 -0600

    Refactor main layout component

--- week9/src/app/modules/main-layout/main-layout.module.ts
+++ week9/src/app/modules/main-layout/main-layout.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ProductsComponent } from './products/products.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MainLayoutComponent } from "./components/main-layout/main-layout.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { ProductsComponent } from "./components/products/products.component";
+import { ProductDetailComponent } from "./components/product-detail/product-detail.component";
 
 @NgModule({
-  declarations: [MainLayoutComponent, ProfileComponent, ProductsComponent, ProductDetailComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    MainLayoutComponent,
+    ProfileComponent,
+    ProductsComponent,
+    ProductDetailComponent
+  ],
+  imports: [CommonModule]
 })
-export class MainLayoutModule { }
+export class MainLayoutModule {}
