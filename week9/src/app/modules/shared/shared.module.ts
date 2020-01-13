e7d08febcb3c8982b1cf17c83ce5fb8044809836
Kiore <ricardochicas95@gmail.com>
2020-01-13 13:42:15 -0600

    Refactor shared component

--- week9/src/app/modules/shared/shared.module.ts
+++ week9/src/app/modules/shared/shared.module.ts
@@ -1,14 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { NavbarComponent } from './navbar/navbar.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @NgModule({
   declarations: [NotFoundComponent, NavbarComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule]
 })
-export class SharedModule { }
+export class SharedModule {}
