cc049c05546adc8e9638e740103fba8c8642ec2e
Kiore <ricardochicas95@gmail.com>
2020-01-13 15:27:11 -0600

    add routes

--- week9/src/app/modules/authentication/authentication.module.ts
+++ week9/src/app/modules/authentication/authentication.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
+import { Routes, RouterModule } from "@angular/router";
 
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  }
+];
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule, LoginComponent, RegisterComponent]
 })
 export class AuthenticationModule {}
