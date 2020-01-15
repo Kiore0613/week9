7f04b7b25f96dcfe9cfdcc4f52abb9a77ec2f376
Kiore <ricardochicas95@gmail.com>
2020-01-15 11:02:35 -0600

    refactor login component

--- week9/src/app/modules/authentication/components/login/login.component.ts
+++ week9/src/app/modules/authentication/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -11,7 +12,11 @@ export class LoginComponent {
   form: FormGroup;
   isDisabled = false;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) {
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
     this.form = this.formBuilder.group({
       email: this.formBuilder.control('', [
         Validators.required,
@@ -23,5 +28,11 @@ export class LoginComponent {
 
   getControl(name: string) {
     return this.form.get(name);
+  }
+
+  login() {
+    this.authService
+      .login(this.form.value)
+      .subscribe(() => this.router.navigate(['']));
   }
 }
