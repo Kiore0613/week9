273e26366693347c7106684eabefaaf0d44e47c9
Kiore <ricardochicas95@gmail.com>
2020-01-15 11:56:43 -0600

    refactor lint

--- week9/src/app/modules/authentication/services/auth.service.ts
+++ week9/src/app/modules/authentication/services/auth.service.ts
@@ -1,21 +1,34 @@
-import { ResponseFromApi } from './../../main-layout/models/responseFromApi';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { User } from '../models/user';
-import { map, tap } from 'rxjs/operators';
+import { ResponseFromApi } from "./../../main-layout/models/responseFromApi";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { User } from "../models/user";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
   private baseUrl: string;
   constructor(private http: HttpClient) {
-    this.baseUrl = 'https://trainee-program.herokuapp.com/api';
+    this.baseUrl = "https://trainee-program.herokuapp.com/api/v1/users/";
   }
 
   login(credentials: User) {
+    const { email, password } = credentials;
+    const data = {
+      data: {
+        email,
+        password
+      }
+    };
+    console.log(data);
     return this.http
-      .post<ResponseFromApi<User>>(`${this.baseUrl}login`, credentials)
-      .pipe(tap(response => response.data));
+      .post<ResponseFromApi<User>>(`${this.baseUrl}login`, data)
+      .pipe(
+        map(response => {
+          console.log(response.data);
+          return response.data;
+        })
+      );
   }
 }
