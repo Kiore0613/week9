0e6b0f7b9e29a66bb5316e8fc5254883efe93e3d
Kiore <ricardochicas95@gmail.com>
2020-01-15 11:00:45 -0600

    add products functionality

--- week9/src/app/modules/main-layout/components/products/products.component.ts
+++ week9/src/app/modules/main-layout/components/products/products.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from "./../../models/product";
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../../services/api.service";
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.scss"]
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
+    this.apiService.getProducts().subscribe(response => {
+      this.products = response;
+      console.log(this.products);
+    });
   }
-
 }
