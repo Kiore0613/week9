1dabeb3cf1d7286f5f53d132c981da37ec407333
Kiore <ricardochicas95@gmail.com>
2020-01-15 11:54:53 -0600

    add map

--- week9/src/app/modules/main-layout/services/api.service.ts
+++ week9/src/app/modules/main-layout/services/api.service.ts
@@ -1,18 +1,18 @@
-import { Product } from "./../models/product";
-import { ResponseFromApi } from "./../models/responseFromApi";
-import { Category } from "./../models/category";
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
+import { Product } from './../models/product';
+import { ResponseFromApi } from './../models/responseFromApi';
+import { Category } from './../models/category';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class ApiService {
   private baseUrl: string;
 
   constructor(private http: HttpClient) {
-    this.baseUrl = "https://trainee-program.herokuapp.com/api/v1";
+    this.baseUrl = 'https://trainee-program.herokuapp.com/api/v1';
   }
 
   getCategories() {
