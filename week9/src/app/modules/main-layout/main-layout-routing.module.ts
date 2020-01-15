8957391c7ea241101bbb7bf8cddbd859eea985e2
Kiore <ricardochicas95@gmail.com>
2020-01-15 15:53:46 -0600

    refactor routes to lazy load

---
+++ week9/src/app/modules/main-layout/main-layout-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductsComponent } from './components/products/products.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products'
+  },
+  {
+    path: 'detail',
+    component: ProductDetailComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MainLayoutRoutingModule {}
