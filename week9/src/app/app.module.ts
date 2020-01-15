26c6b6951a42d30e70cc1ce46d6203e227018b98
Kiore <ricardochicas95@gmail.com>
2020-01-15 17:54:18 -0600

    add Ngrx

--- week9/src/app/app.module.ts
+++ week9/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { AuthenticationModule } from './modules/authentication/authentication.module';
-import { LoginComponent } from './modules/authentication/components/login/login.component';
-import { MainLayoutModule } from './modules/main-layout/main-layout.module';
-import { SharedModule } from './modules/shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { AuthenticationModule } from "./modules/authentication/authentication.module";
+import { MainLayoutModule } from "./modules/main-layout/main-layout.module";
+import { SharedModule } from "./modules/shared/shared.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { StoreModule } from "@ngrx/store";
+import { reducers, metaReducers } from "./store/reducers";
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +18,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     SharedModule,
     MainLayoutModule,
-    AuthenticationModule
+    AuthenticationModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
