7986998f7ea02024ad6a6c48b5d80c4322da917a
Kiore <ricardochicas95@gmail.com>
2020-01-15 15:55:16 -0600

    refactor functionality to add filter by category

--- week9/src/app/modules/main-layout/components/products/products.component.ts
+++ week9/src/app/modules/main-layout/components/products/products.component.ts
@@ -1,21 +1,36 @@
-import { Product } from './../../models/product';
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../services/api.service';
+import { Product } from "./../../models/product";
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../../services/api.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.scss"]
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.apiService.getProducts().subscribe(response => {
-      this.products = response;
-      console.log(this.products);
+    this.activatedRoute.queryParamMap.subscribe(param => {
+      let categoryId = +param.get("category");
+      if (categoryId) {
+        this.apiService.getProductsById(categoryId).subscribe(response => {
+          this.products = response;
+          console.log(response);
+        });
+        console.log("inside if");
+      } else {
+        this.apiService
+          .getProducts()
+          .subscribe(response => (this.products = response));
+        console.log("inside else");
+      }
     });
   }
 }
