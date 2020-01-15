8306aac4683b1c9308fe1f77906cffead20fbd99
Kiore <ricardochicas95@gmail.com>
2020-01-15 11:02:19 -0600

    create login method

--- week9/src/app/modules/authentication/services/auth.service.ts
+++ week9/src/app/modules/authentication/services/auth.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from "@angular/core";
+import { ResponseFromApi } from './../../main-layout/models/responseFromApi';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class AuthService {
   private baseUrl: string;
-  constructor() {
-    this.baseUrl = "https://trainee-program.herokuapp.com/api";
+  constructor(private http: HttpClient) {
+    this.baseUrl = 'https://trainee-program.herokuapp.com/api';
+  }
+
+  login(credentials: User) {
+    return this.http
+      .post<ResponseFromApi<User>>(`${this.baseUrl}login`, credentials)
+      .pipe(tap(response => response.data));
   }
 }
