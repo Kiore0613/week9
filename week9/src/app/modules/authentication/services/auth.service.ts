8d845842c59ac01e8ac41254db31df95ad4ecd5b
Kiore <ricardochicas95@gmail.com>
2020-01-15 00:14:44 -0600

    create auth service

--- week9/src/app/modules/authentication/services/auth.service.ts
+++ week9/src/app/modules/authentication/services/auth.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
-
-  constructor() { }
+  private baseUrl: string;
+  constructor() {
+    this.baseUrl = "https://trainee-program.herokuapp.com/api";
+  }
 }
