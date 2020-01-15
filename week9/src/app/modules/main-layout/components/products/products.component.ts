c2fb569858173ebb6745becd17c45c6de16a0fbd
Kiore <ricardochicas95@gmail.com>
2020-01-15 11:56:29 -0600

    destructuring data to email

--- week9/src/app/modules/main-layout/components/products/products.component.ts
+++ week9/src/app/modules/main-layout/components/products/products.component.ts
@@ -1,11 +1,11 @@
-import { Product } from "./../../models/product";
-import { Component, OnInit } from "@angular/core";
-import { ApiService } from "../../services/api.service";
+import { Product } from './../../models/product';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
-  selector: "app-products",
-  templateUrl: "./products.component.html",
-  styleUrls: ["./products.component.scss"]
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
