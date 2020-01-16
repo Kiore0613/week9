398d4525bf7e49a72d63983377127fade18ed327
Kiore <ricardochicas95@gmail.com>
2020-01-16 16:14:47 -0600

    refactor redirect to products in navbar

--- week9/src/app/modules/shared/components/navbar/navbar.component.ts
+++ week9/src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,16 +1,25 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent {
-  constructor() {}
+  constructor(private router: Router) {}
 
   @Output() toggle = new EventEmitter();
+  @Output() search = new EventEmitter();
 
   showMenu() {
     this.toggle.emit();
+  }
+  searchProduct(name: string) {
+    this.search.emit(name);
+  }
+
+  redirectTo() {
+    this.router.navigate(["/main"]);
   }
 }
