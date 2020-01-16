b2e73e8887d91aea30f03b9f1129b3f8a91c9aa7
Kiore <ricardochicas95@gmail.com>
2020-01-16 01:14:13 -0600

    remove routes

--- week9/src/app/app-routing.module.ts
+++ week9/src/app/app-routing.module.ts
@@ -1,40 +1,29 @@
-import { LoginComponent } from './modules/authentication/components/login/login.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { NotFoundComponent } from './modules/shared/components/not-found/not-found.component';
-import { MainLayoutComponent } from './modules/main-layout/components/main-layout/main-layout.component';
+import { LoginComponent } from "./modules/authentication/components/login/login.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { NotFoundComponent } from "./modules/shared/components/not-found/not-found.component";
+import { MainLayoutComponent } from "./modules/main-layout/components/main-layout/main-layout.component";
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'main'
+    path: "",
+    pathMatch: "full",
+    redirectTo: "main"
   },
   {
-    path: 'main',
+    path: "main",
     component: MainLayoutComponent,
-    /*children: [
-      {
-        path: "detail",
-        component: ProductDetailComponent
-
-      },
-      {
-        path: "products",
-        component: ProductsComponent
-      }
-    ]*/
     loadChildren: () =>
       import(`./modules/main-layout/main-layout.module`).then(
         m => m.MainLayoutModule
       )
   },
   {
-    path: 'login',
+    path: "login",
     component: LoginComponent
   },
   {
-    path: '**',
+    path: "**",
     component: NotFoundComponent
   }
 ];
