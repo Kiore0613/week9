b44f5cab7e3c6da1ada0577a43a0c5b1e282aa9b
Kiore <ricardochicas95@gmail.com>
2020-01-16 12:05:46 -0600

    create toast component

---
+++ week9/src/app/modules/shared/components/toast/toast.component.ts
@@ -0,0 +1,10 @@
+import { MatSnackBar, MAT_SNACK_BAR_DATA } from "@angular/material/snack-bar";
+import { Component } from "@angular/core";
+import { inject } from "@angular/core/testing";
+
+@Component({
+  selector: "app-toast",
+  templateUrl: "./toast.component.html",
+  styleUrls: ["./toast.component.scss"]
+})
+export class ToastComponent {}
