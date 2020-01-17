aac32baae38b922bf543f226d9435ac2f1abc934
Kiore <ricardochicas95@gmail.com>
2020-01-17 11:23:09 -0600

    add functionality onInit

--- week9/src/app/modules/main-layout/components/product-detail/product-detail.component.ts
+++ week9/src/app/modules/main-layout/components/product-detail/product-detail.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ApiService } from "./../../services/api.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Product } from "../../models/product";
 
 @Component({
-  selector: 'app-product-detail',
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  selector: "app-product-detail",
+  templateUrl: "./product-detail.component.html",
+  styleUrls: ["./product-detail.component.scss"]
 })
-export class ProductDetailComponent {
-  constructor() {}
+export class ProductDetailComponent implements OnInit {
+  productDetail: Product = null;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit() {
+    const product = this.activatedRoute.snapshot.paramMap.get("slug");
+    this.apiService.getProductDetail(product).subscribe(response => {
+      this.productDetail = response;
+      console.log(this.productDetail);
+    });
+  }
 }
