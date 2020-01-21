bf5da0d7531683f557fac47de9eb712c33cafd3a
Kiore <ricardochicas95@gmail.com>
2020-01-21 16:23:33 -0600

    create login test

--- src/app/modules/authentication/components/login/login.component.spec.ts
+++ src/app/modules/authentication/components/login/login.component.spec.ts
@@ -1,16 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { LoginComponent } from './login.component';
+import { LoginComponent } from "./login.component";
+import { FormBuilder, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
+import { ToastService } from "src/app/core/services/toast.service";
+import { CommonModule } from "@angular/common";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterModule } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { MainLayoutComponent } from "src/app/modules/main-layout/components/main-layout/main-layout.component";
+import { SharedModule } from "src/app/modules/shared/shared.module";
+import { MainLayoutModule } from "src/app/modules/main-layout/main-layout.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { Button } from 'protractor';
 
-describe('LoginComponent', () => {
+fdescribe("LoginComponent", () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [
+        BrowserAnimationsModule,
+        MainLayoutModule,
+        SharedModule,
+        CommonModule,
+        MatInputModule,
+        MatButtonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([
+          { path: "", component: MainLayoutComponent }
+        ])
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +44,23 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  fit("should create a form with 2 controls", () => {
+    expect(component.form.contains("email")).toBeTruthy();
+    expect(component.form.contains("password")).toBeTruthy();
+  });
+
+  fit("should make the controls required", () => {
+    let controlEmail = component.form.get("email");
+    let controlPassword = component.form.get("password");
+
+    controlEmail.setValue("");
+    controlPassword.setValue("");
+
+    expect(controlEmail.valid).toBeFalsy();
+    expect(controlPassword.valid).toBeFalsy();
+  });
+
+  fit("should create a button", () => {
+    expect(fixture.debugElement.nativeElement.querySelector("button")).toBeTruthy();
   });
 });
